ffbbdebb862901d38d14116ff40500baa8b803df
altantur <turuu.br@gmail.com>
2020-11-11 15:32:07 +0800

    Added scheduled job:

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", {structuredData: true});
     response.send("Hello from Firebase!");
 });
+
+exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+    console.log('This will be run every minutes!');
+    return null;
+});
+
