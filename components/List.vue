20ca9b19b178a3e129846930e68a7cc5a5d0714e
Badarch <45225030+stardollarstar@users.noreply.github.com>
2020-11-12 15:24:34 +0800

    add some style and removed time counter

--- components/List.vue
+++ components/List.vue
@@ -1,68 +1,64 @@
 <template>
   <div id="container-box">
-    <div>{{data.staff}}</div>
-    <div>{{data.car}}</div>
-    <div>{{data.id}}</div>
+    <div>Staff name here</div>
+    <div>Car model here</div>
+    <div>Id number of car here</div>
     <div id="timer-container">
       <svg class="timer-svg">
-          <circle cx="50" cy="50" r="47" class="circle" id="circle-of-timer"></circle>
+          <circle cx="40" cy="40" r="37" class="circle" id="circle-of-timer"></circle>
       </svg>
-      <div id="time-text">0:0</div>
+      <div id="time-text">Time</div>
     </div>
-    <div>{{data.comment}}</div>
+    <div>Comments here</div>
   </div>
 </template>
 
 <script>
 export default {
     mounted: function() {
-      let timeleft = 10
+      let timeleft = 1800
       let temp = timeleft;
       let LoC = 295.16 / timeleft;
-      setInterval(() => {
-        if(temp >= 0) {
-          document.getElementById("time-text").innerHTML = temp;
-          document.getElementById("circle-of-timer").style.strokeDasharray = LoC * (temp - 1) + "," + LoC * (timeleft - temp + 1);
-          temp--;
-        }
-      }, 1000);
-    },
-    props: {
-        data: Object
+      // setInterval(() => {
+      //   if(temp >= 0) {
+      //     document.getElementById("time-text").innerHTML = temp;
+      //     document.getElementById("circle-of-timer").style.strokeDasharray = LoC * (temp - 1) + "," + LoC * (timeleft - temp + 1);
+      //     temp--;
+      //   }
+      // }, 1000);
     }
 }
 </script>
 
 <style>
 #container-box {
-    width: 200px;
-    border: 1px solid black;
+    width: 100%;
     display: flex;
     align-items: center;
     flex-direction: column;
 }
 .timer-svg {
-  width: 100px;
-  height: 100px;
+  width: 80px;
+  height: 80px;
 }
 .circle {
   fill: white;
   stroke-width: 3px;
-  stroke: black;
-  stroke-dasharray: 295.16, 0;
+  stroke: navy;
+  stroke-dasharray: 232.58, 0;
   transform: rotate(-90deg);
   transform-origin: center;
   transition: 1s all linear;
 }
-.circle:hover {
-  stroke-dasharray: 0, 295.16;
-}
 #time-text {
-  width: 100px;
-  height: 100px;
-  margin-top: -100px;
+  width: 80px;
+  height: 80px;
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 12px;
 }
 </style>
