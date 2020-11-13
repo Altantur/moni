cd230df4ae81363f9fb95e90f647ffafd4da5bb4
altantur <turuu.br@gmail.com>
2020-11-13 18:08:16 +0800

    TODO on 14th November

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,6 +1,31 @@
 <template>
     <div class="min-h-screen">
-    Hello
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title>User Management</v-list-item-title>
+        <v-list-item-subtitle>Үйлчилгээний ажилтан</v-list-item-subtitle>
+        <v-list-item-subtitle>Засварчин</v-list-item-subtitle>
+        <v-list-item-subtitle>Админ</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Vehicle management</v-list-item-title>
+        <v-list-item-subtitle>Цагыг оруулах</v-list-item-subtitle>
+        <v-list-item-subtitle>Улсын дугаар оруулах машины</v-list-item-subtitle>
+        <v-list-item-subtitle>Засварчиныг тохируулах</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title>Admin management</v-list-item-title>
+        <v-list-item-subtitle>Пасспорт солих</v-list-item-subtitle>
+        <v-list-item-subtitle>Хугацаагаар шүүж татаж авах </v-list-item-subtitle>
+        <v-list-item-subtitle>Үйлчилгээний ажилтантай адилхан үйлдэл хийх боломжтой</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
     </div>
 </template>
 
