0cc8354fee01a3525e31db57a82c708db8c9130e
Badarch <45225030+stardollarstar@users.noreply.github.com>
2020-11-16 14:10:34 +0800

    used dummy targets

--- pages/queue.vue
+++ pages/queue.vue
@@ -2,13 +2,14 @@
     <div id="Q-container">
         <div id="header">
             <img src="/logo.jpg">
+            <button id="test" @click="test">cancel</button>
         </div>
         <div id="body-container">
             <h1 class="h-one">Queue</h1>
             <div id="list-container">
-                <h2 class="h-two">In progress</h2>
-                <div v-for="item in arr" :key="item">
-                    <Qlist />
+                <h2 class="h-two">Active</h2>
+                <div v-for="item in object" :key="item">
+                    <Qlist class="listed-items" :data="item" :active="arr"/>
                 </div>
             </div>
         </div>
@@ -18,8 +19,47 @@
 export default {
     data() {
         return {
-            arr: [0, 1, 2, 3]
+            arr: [0, 1, 2, 3],
+            object: [
+                    {
+                        name: "Munkhjargal",
+                        model: "Land200",
+                        status: "Waiting",
+                        time: "12:05"
+                    },
+                    {
+                        name: "Badarch",
+                        model: "LX570",
+                        status: "Waiting",
+                        time: "13:30"
+                    },
+                    {
+                        name: "Temuujin",
+                        model: "LX470",
+                        status: "Waiting",
+                        time: "10:10"
+                    },
+                    {
+                        name: "Bataa",
+                        model: "Land100",
+                        status: "Waiting",
+                        time: "11:00"
+                    }
+            ]
         }
+    },
+    methods:{
+        async fetchWeather() {
+            const elems = await this.$axios.$get('http://api.openweathermap.org/data/2.5/weather?q=Ulaanbaatar&APPID=454e0e864b03e71b886b41d8103e6faf');
+            console.log(elems);
+        },
+        test() {
+            document.getElementsByClassName("status-box")[1].innerHTML = "Canceled";
+            document.getElementsByClassName("listed-items")[1].classList.add("canceled");
+        }
+    },
+    mounted() {
+        this.fetchWeather();
     }
 }
 </script>
@@ -30,7 +70,9 @@ body {
 </style>
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Hind+Siliguri&display=swap');
-
+#test {
+    color: white;
+}
 #header{
     width: 100%;
     height: 150px;
@@ -48,6 +90,9 @@ body {
     color: rgba(0, 0, 0, 0.8);
     width: 50%;
 }
+.canceled {
+    opacity: 0.4;
+}
 #list-container {
     width: 50%;
     padding-left: 20px;
@@ -60,5 +105,8 @@ body {
     color: rgba(0, 0, 0, 0.7);
     width: 100%;
     margin-bottom: 10px;
+}
+.canceled {
+    opacity: 0.4;
 }
 </style>
