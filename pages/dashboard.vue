5af81a03690831a853c78a54ca2628eb4d35ce1f
altantur <turuu.br@gmail.com>
2020-11-17 20:59:42 +0800

    Separated dialog from dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -60,89 +60,12 @@
         <TimeLine />
       </v-tab-item>
     </v-tabs>
-    <v-dialog
-      v-model="dialog"
-      max-width="500px"
-    >
-      <template #activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          class="mb-2"
-          v-bind="attrs"
-          v-on="on"
-        >
-          Шинэ захиалга нэмэх
-        </v-btn>
-      </template>
-      <v-card>
-        <v-card-title>
-          <span class="headline">{{ formTitle }}</span>
-        </v-card-title>
-
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-text-field
-                  v-model="editedItem.vecNumber"
-                  label="Машины дугаар"
-                />
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-combobox
-                  v-model="editedItem.role"
-                  :items="zausers"
-                  label="Засварын ажилтан"
-                />
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-combobox
-                  v-model="editedItem.role"
-                  :items="uzusers"
-                  label="Үйлчилгээний зөвлөх"
-                />
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="close"
-          >
-            Болих 
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="save"
-          >
-            Хадгалах
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapActions } from 'vuex'
+
 export default {
     layout: 'dashboard',
     middleware: 'authenticated',
@@ -154,127 +77,41 @@ export default {
         text: "39мин", // Time left
         comment: "Дугуй сольж байна.", // Time left
       },
-      dialog: false,
-      loading: true,
-      dialogDelete: false,
-      editedIndex: -1,
-      uzusers: [],//Үйлчилгээний зөвлөх
-      zausers: [],//Засварын ажилтан
-      editedItem: {
-        vecNumber: '',
-        duration: '',
-        comment: '',
-      },
-      defaultItem: {
-        vecNumber: '',
-        duration: '',
-        comment: '',
-      },
+      users: [],
+      loading: false,
     }),
     computed: {
-      ...mapState({
+      ...mapState ({
         authUser: (state) => state.authUser,
       }),
-      ...mapGetters({
+      ...mapGetters ({
         isLoggedIn: 'isLoggedIn',
       }),
-      formTitle () {
-        return this.editedIndex === -1 ? 'Захиалга нэмэх' : 'Засах'
-      },
-    },
-    watch: {
-      dialog (val) {
-        val || this.close()
-      },
-      dialogDelete (val) {
-        val || this.closeDelete()
-      },
     },
     mounted () {
         this.$fire.database.ref('message').on('value', snapshot => {
             this.message = snapshot.val().message;
         })
     },
+    created () {
+      this.initialize()
+    },
     methods: {
+      ...mapActions ([
+        'setUsers'
+      ]),
+      initialize () {
+        this.loading = true
+        const ref = this.$fire.database.ref('users')
+        ref.on('value', (data) => {
+          if (data.val())
+          this.users = Object.values(data.val())
+          this.loading = false
+          this.setUsers (this.users)
+        });
+      },
       create () {
-        this.dialog = true
-      },
-      editItem (item) {
-        this.editedIndex = this.users.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        console.log(item);
-        this.dialog = true
-      },
-
-      deleteItem (item) {
-        this.editedIndex = this.users.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialogDelete = true
-      },
-
-      deleteItemConfirm () {
-        this.loading = true;
-        const delUser = this.$fire.functions.httpsCallable('deleteUser')
-        delUser(this.editedItem).then((result) => {
-          this.users.splice(this.editedIndex, 1)
-          console.log('Success',result);
-          this.loading = false;
-        }).catch((error) => {
-          console.log(error)
-          this.loading = false;
-        });
-        this.closeDelete()
-      },
-
-      close () {
-        this.dialog = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      closeDelete () {
-        this.dialogDelete = false
-        this.$nextTick(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        })
-      },
-
-      saveToDB (url) {
-        if (url) this.editedItem.url = url
-        const saveUser = this.$fire.functions.httpsCallable('save')
-        saveUser(this.editedItem).then((result) => {
-          console.log('Success',result);
-          this.loading = false;
-        }).catch((error) => {
-          console.log(error)
-          this.loading = false;
-        });
-        if (this.editedIndex > -1) {
-          Object.assign(this.users[this.editedIndex], this.editedItem)
-        } else {
-          this.users.push(this.editedItem)
-        }
-        this.close()
-      },
-
-      save () {
-        this.loading = true;
-        if (this.editedItem.photoURL) {
-          const ref = this.$fire.storage.ref().child(this.editedItem.email + '/' + this.editedItem.photoURL.name)
-          ref.put(this.editedItem.photoURL).then((snapshot) => {
-            ref.getDownloadURL().then((url) => {
-              this.saveToDB (url)
-            }).catch((error) => {
-              console.log(error)
-              this.loading = false;
-            });
-          });
-        } else {
-          this.saveToDB(null);
-        }
+        alert()
       },
       async writeToRealtimeDb() {
           const messageRef = this.$fire.database.ref('message')
